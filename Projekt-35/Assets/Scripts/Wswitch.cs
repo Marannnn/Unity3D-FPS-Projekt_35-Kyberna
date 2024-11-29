cdc69e356ac58382d6111c7f5efea02d4f9e7042
vaclav08 <lubas.vaclav@ssakhk.cz>
2024-11-29 10:54:11 +0100

    animace
    dokončení lopaty a revolveru

--- Projekt-35/Assets/Scripts/Wswitch.cs
+++ Projekt-35/Assets/Scripts/Wswitch.cs
@@ -31,6 +31,22 @@ public class Wswitch : MonoBehaviour
             else
                 SelectedWeapon--;
         }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SelectedWeapon = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2) && transform.childCount >= 2)
+        {
+            SelectedWeapon = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3) && transform.childCount >= 3)
+        {
+            SelectedWeapon = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4) && transform.childCount >= 4)
+        {
+            SelectedWeapon = 3;
+        }
         if (previusSelectedWeapon != SelectedWeapon)
         {
             SelectWeapon();
